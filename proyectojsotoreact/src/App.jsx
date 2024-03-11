399fef85fa1a0588239b5469e79559ff8b6fe4c5
jennifersoto1989 <139190189+jennifersoto1989@users.noreply.github.com>
2024-03-11 00:18:06 -0300

    avabces

--- proyectojsotoreact/src/App.jsx
+++ proyectojsotoreact/src/App.jsx
@@ -3,16 +3,21 @@ import './App.css'
 import NavBar from './components/Nav.Bar/Nav.Bar';
 /*import ItemListContainer from './components/ItemListContainer/ItemListContainer';*/
 import Hero from './components/Hero/Hero';
+import Productos from './components/Productos/Productos';
+import Footer from './components/Footer/Footer';
+
+
+
 
 
 function App() {
   return (
     <>
-  
+           
   <NavBar/>
-  <Hero/>
- 
-
+    <Hero/>
+  <Productos/>
+  <Footer/>
 </>
    
 )
--- proyectojsotoreact/src/components/Hero/Hero.jsx
+++ proyectojsotoreact/src/components/Hero/Hero.jsx
@@ -1,37 +1,23 @@
 import React from 'react'
-import Carousel from 'react-bootstrap/Carousel';
+import Card from 'react-bootstrap/Card';
 
 
 
-const Hero = () => {
+function Hero() {
   return (
-    <Carousel>
-      <Carousel.Item>
-        <ExampleCarouselImage text="First slide" />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <ExampleCarouselImage text="Second slide" />
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <ExampleCarouselImage text="Third slide" />
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
+    <>
+      <Card>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Text>
+          "Imagen slider"
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      
+    
+    </>
   );
 }
 
-
-export default Hero
+export default Hero;
